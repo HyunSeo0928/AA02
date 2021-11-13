3deb094e7d7144b2ff7776ac52a0b20178592539
ar02 <79964404+HyunSeo0928@users.noreply.github.com>
2021-11-13 16:49:00 +0900

    Add files via upload

---
+++ aa02-rpt02/start/hello_function.js
@@ -0,0 +1,6 @@
+function hello(what) {
+    console.log("Hello " + what + " !");
+}
+
+hello("aa02");
+hello("HyunSeo0928, 김현서");
---
+++ aa02-rpt02/start/index_aa02.js
@@ -0,0 +1,7 @@
+//index_aa02.js
+
+var myinfo = require('./aa02info');
+
+myinfo("aa04", "HyunSeo0928", '010-3858-9204');
+
+myinfo("aa02", "Comsi", '010-3858-9204');
